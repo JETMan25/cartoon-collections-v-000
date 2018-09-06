1bcb88719b421790f4a8aef604740df1b6fc3fbb
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 14:53:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ dwarves.each.with_index(1) do |dwarf, index|
   end
   end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planateer_calls)
+  planateer_calls.map {|call| call.capualize + "!"}
+ 
 end
 
 def long_planeteer_calls# code an argument here
