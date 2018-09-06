3236fb26466a91e6313645a54c571dcb24a9d0c9
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 15:00:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,9 @@ def long_planeteer_calls(planateer_calls)
 
 def find_the_cheese(cheese)
   cheeses = %w(gouda cheddar camembert)
+  
   cheese.find do |maybe|
     cheese.include?(maybe)
-  #cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
 end
