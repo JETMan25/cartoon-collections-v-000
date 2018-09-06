cb9bb518ab7d8314f3159e3947cd21fc1d6c5d7f
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 15:04:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,19 +13,13 @@ def long_planeteer_calls(planateer_calls)
   planateer_calls.any? {|call| call.length > 4}
   end
 
-def find_the_cheese(potentially_cheesy_items)
+def find_the_cheese(cheese)
   cheeses = %w(gouda cheddar camembert)
   
-    potentially_cheesy_items.find do |maybe_cheese|
+    cheese.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   
 end
 end
 
-# def find_the_cheese(potentially_cheesy_items)
-#   cheeses = %w(gouda cheddar camembert)
 
-#   potentially_cheesy_items.find do |maybe_cheese|
-#     cheeses.include?(maybe_cheese)
-#   end
-# end
