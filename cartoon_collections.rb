aa3e8ec6f5f94c9da2b6887ab54c0d137760d1f3
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 14:56:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planateer_calls)
 end
 
 def long_planeteer_calls(planateer_calls)
-  planateer_calls
+  planateer_calls.any? {|call| call.length > 4}
   # Your code here
 end
 
